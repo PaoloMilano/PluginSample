6e26e916c8d339965ca0a7c03ca0cd6a5466ac14
Paolo Milano <spacitron999@gmail.com>
2020-06-08 17:50:36 +0200

    Added some comments and moved function to separate file

--- buildSrc/src/main/java/ColorsTask.kt
+++ buildSrc/src/main/java/ColorsTask.kt
@@ -9,11 +9,13 @@ internal open class ColorsTask : DefaultTask(){
     @get:OutputFile
     lateinit var outputFile: File
 
+    // We now take a file and will therefore change the annotation to reflect this
     @get:InputFile
     lateinit var inputFile: File
 
     @TaskAction
     fun makeResources() {
+        // Simple CSV parsing mechanism. For more complex use cases we may want to use a dedicated library
         inputFile.readLines().map { color ->
             color.split(",")
         }.joinToString {
--- buildSrc/src/main/java/MyFirstPlugin.kt
+++ buildSrc/src/main/java/MyFirstPlugin.kt
@@ -1,5 +1,4 @@
 import extensions.android
-import extensions.getAllPossibleFileLocationsByVariantDirectory
 import extensions.variants
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
@@ -9,24 +8,23 @@ import java.io.File
 internal class MyFirstPlugin : Plugin<Project> {
 
     companion object {
+        // All clients of this plugin will need to use this specific file name
         private const val COLOR_FILE_NAME = "my_colors.txt"
     }
 
     override fun apply(project: Project) {
         project.android().variants().all { variant ->
 
-            // Make a task for each combination of build type and product flavor
             val colorTaskName = "generateColors${variant.name.capitalize()}"
-
             val outputPath = "${project.buildDir}/generated/res"
 
-            // Register a simple task as a lambda. We'll later move it its own class and add some niceties
             project.tasks.register(colorTaskName, ColorsTask::class.java) { colorTask ->
                 colorTask.group = "MyPluginTasks"
 
                 val colorsFileLocations =
                     getAllPossibleFileLocationsByVariantDirectory(variant.dirName)
 
+                // Check that whether the file declared in any of the relevant folders and throw an exception otherwise
                 val colorsFile = colorsFileLocations.asSequence()
                     .map { project.file("$it/$COLOR_FILE_NAME") }
                     .firstOrNull { it.isFile }
@@ -35,14 +33,13 @@ internal class MyFirstPlugin : Plugin<Project> {
                                 "\n${colorsFileLocations.joinToString("\n")}"
                     )
 
-                // We write our output in the build folder. Also note that we want to have a
-                // reference to this so we can later mark it as a generated resource folder
                 val outputDirectory =
                     File("$outputPath/${variant.dirName}").apply { mkdir() }
                 colorTask.outputFile = File(outputDirectory, "values/generated_colors.xml")
+
+                // Finally set the file on the Task object
                 colorTask.inputFile = colorsFile
 
-                // Marks the output directory as an app resource folder
                 variant.registerGeneratedResFolders(
                     project.files(outputDirectory).builtBy(
                         colorTask
